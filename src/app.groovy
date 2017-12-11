b23a11ce088b4a1dd69e72f43705168787f09aed
TAKUYAA <takuyaa@jp.ibm.com>
2017-12-11 11:19:07 +0900

    View HTML

--- src/app.groovy
+++ src/app.groovy
@@ -2,6 +2,10 @@
 class App {
 	@RequestMapping("/")
 	def home() {
-		"Hello World"
+		def header = "<html><body>"
+		def footer = "</body></html>"
+		def content = "<h1>Hello World!</h1><p>this is the html content."
+
+		header + content + footer
 	}
 }
